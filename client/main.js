557aa9f8ec2ca15d54ebd5c9c23b195bf431e0c1
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-12-25 14:38:05 +0530

    changed url

--- client/main.js
+++ client/main.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.querySelector(`#${uniqueID}`);
   loader(messageDiv)
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://codeapicodex.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
